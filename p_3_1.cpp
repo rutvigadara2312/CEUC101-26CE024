ec06fc6c64f4b4b3da2baf77e97255e9c0038a13
rutvigadara2312 <rutvigadara2312@gmail.com>
2026-09-08 19:10:39 +0530

    Add files via upload

---
+++ p_3_1.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+ {
+    string enrollment_number, name, branch;
+    int semester,mobile_number,m1, m2, m3;
+    float average_marks, total_marks, percentage;
+
+
+    cout<<"***********************************************\n"<<endl;
+    cout<<" Student Record Management System   \n"<<endl;
+    cout<<"***********************************************\n"<<endl;
+
+    //Taking student information as an input;
+
+    cout<<"-----------------------------------------------\n"<<endl;
+    cout<<" Student Registration   \n"<<endl;
+    cout<<"-----------------------------------------------\n"<<endl;
+
+
+    cout<<left<<setw(25)<<"Enter Enrollment Number"<<":";
+    cin>>enrollment_number;
+    cin.ignore();
+    cout<<left<<setw(25)<<"Enter Student Name"<<":";
+    getline(cin,name);
+    cout<<left<<setw(25)<<"Enter branch"<<":";
+    cin>>branch;
+    cout<<left<<setw(25)<<"Enter semester"<<":";
+    cin>>semester;
+    cout<<left<<setw(25)<<"Enter Mobile Number"<<":";
+    cin>>mobile_number;
+
+    cout<<"--------------------------------------------------\n"<<endl;
+    cout<<" Academic Registration   \n"<<endl;
+    cout<<"--------------------------------------------------\n"<<endl;
+
+    //taking subject marks as an input;
+
+    cout<<left<<setw(50)<<"Enter Maths Marks"<<":";
+    cin>>m1;
+    cout<<left<<setw(50)<<"Enter physics Marks"<<":";
+    cin>>m2;
+    cout<<left<<setw(50)<<"Enter computer programming foundation Marks"<<":";
+    cin>>m3;
+
+    //calculating Total marks, Average marks and Percentage;
+
+    total_marks=m1+m2+m3;
+    average_marks= (m1+m2+m3)/3;
+    percentage= (m1+m2+m3)/3;
+
+    //Display Total marks, Average marks and Percentage;
+
+    cout<<"-----------------------------------------------------\n"<<endl;
+    cout<<" Academic Summary   \n"<<endl;
+    cout<<"-----------------------------------------------------\n"<<endl;
+
+    cout<<left<<setw(20)<<"Total Marks"<<":"<<total_marks<<endl;
+    cout<<left<<setw(20)<<"Average Marks"<<":"<<average_marks<<endl;
+    cout<<left<<setw(20)<<"Percentage"<<":"<<percentage<<endl;
+
+    cout<<"-----------------------------"<<endl;
+    cout<<"     Student Information     "<<endl;
+    cout<<"-----------------------------"<<endl;
+
+    cout<<left<<setw(20)<<"Enrollment Number"<<":"<<enrollment_number<<endl;
+    cout<<left<<setw(20)<<"Student Name"<<":"<<name<<endl;
+    cout<<left<<setw(20)<<"Branch"<<":"<<branch<<endl;
+    cout<<left<<setw(20)<<"Semester"<<":"<<semester<<endl;
+    cout<<left<<setw(20)<<"Mobile Number"<<":"<<mobile_number<<endl;
+
+
+
+
+
+
+
+    return 0;
+ }
